c54f8b4fca7ef328240b4a82ffb77deedc81d1aa
daruvurisowmya <109509763+daruvurisowmya@users.noreply.github.com>
2022-07-18 21:35:59 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,11 @@
+'''
+Name:sowmya
+Date & Day:18th July 2022 (Monday)
+File Desc:Types of comments in python
+          1)single line comment
+          2)multi line comment  
+'''
+# its a single line comment 
+print('hy today we have seen comments in python')
+
+
